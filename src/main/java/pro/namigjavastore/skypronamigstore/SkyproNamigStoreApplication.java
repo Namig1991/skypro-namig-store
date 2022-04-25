970262611aca97196fe4e817b860d43827609a60
Namig1991 <safarovnamig09@gmail.com>
2022-04-25 14:02:25 +0300

    Первый коммит дз 2.11

---
+++ src/main/java/pro/namigjavastore/skypronamigstore/SkyproNamigStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.namigjavastore.skypronamigstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproNamigStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproNamigStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/namigjavastore/skypronamigstore/SkyproNamigStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.namigjavastore.skypronamigstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproNamigStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
